a035cd520b2bd1b17717b1860c1d918a3d094bcd
Joe Rickerby <joerick@mac.com>
2015-06-02 21:11:34 +0100

    Add some setup to build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,34 @@
 
 set -x
 set -o errexit
+set -o pipefail
 
 sudo apt-get -y update
 sudo apt-get -y install python-pip
+sudo apt-get -y install avahi-daemon
+
+# install tbprocessd
+(
+  git clone https://github.com/tingbot/tbprocessd.git
+  cd tbprocessd
+  sudo make install
+  cd ..
+  sudo rm -rf tbprocessd
+)
+
+# install avahi-daemon config
+(
+    sudo tee /etc/avahi/services/tingbot.service > /dev/null <<EOF
+<?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+<!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+<service-group>
+  <name replace-wildcards="yes">%h</name>
+  <service>
+    <type>_tingbot-ssh._tcp</type>
+    <port>22</port>
+  </service>
+</service-group>
+EOF
+)
+
+sudo REPO_URI=https://github.com/notro/rpi-firmware rpi-update 4815829b3f98e1b9c2648d9643dfe993054923ce
