e77d216d29affed8a10f143a215ea47cae0b6c06
Joe Rickerby <joerick@mac.com>
2016-03-04 15:22:54 +0000

    Whitespace

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 set -o pipefail
 
 sudo apt-get -y update
-sudo apt-get -y install python-pip python-dev 
+sudo apt-get -y install python-pip python-dev
 sudo apt-get -y install avahi-daemon evtest tslib libts-bin
 
 sudo pip install pillow==2.9.0 requests==2.7.0 evdev==0.5.0
