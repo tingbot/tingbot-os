2b23dd84e0d52b3c430c4341bfddff6694d8f366
Joe Rickerby <joerick@mac.com>
2016-03-06 22:33:00 +0000

    Fix build system to build on the new jessie image

---
+++ patch-img.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import mmap, argparse
+
+parser = argparse.ArgumentParser(description='Inplace find/replace a file.')
+
+parser.add_argument('find', type=str)
+parser.add_argument('replace', type=str)
+parser.add_argument('file', type=argparse.FileType('r+b'))
+
+args = parser.parse_args()
+
+if len(args.find) != len(args.replace):
+    raise Exception("'find' and 'replace' must be the same length")
+
+try:
+    s = mmap.mmap(args.file.fileno(), 0)
+
+    offset = s.find(args.find)
+
+    if offset == -1:
+        raise Exception("'find' not found in file.")
+
+    s[offset:offset+len(args.replace)] = args.replace
+finally:
+    s.close()
+    args.file.close()
