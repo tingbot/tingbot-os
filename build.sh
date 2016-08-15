b6700d4d512cf30490f197e57b7654f8e76f6096
Joe Rickerby <joerick@mac.com>
2016-08-15 21:53:33 +0100

    Run expand-rootfs on first boot

--- build.sh
+++ build.sh
@@ -8,3 +8,6 @@ sudo apt-get -y install gdebi-core
 
 sudo gdebi --non-interactive libsdl1.2debian_1.2.15-5_armhf.deb
 sudo gdebi --non-interactive tingbot-os.deb
+
+# run the expand-rootfs tingapp on first boot (the app sets the startup symlink back after running)
+sudo ln -snf ../usr/share/tingbot/expand-rootfs.tingapp /apps/startup
