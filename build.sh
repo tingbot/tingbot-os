27dbb2b2dc2b5ddc09a087bbb8776f5e8337f78c
Joe Rickerby <joerick@mac.com>
2016-08-17 14:48:18 +0100

    Remove Debs after installation so they're not in $HOME when user logs in

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ sudo apt-get -y install gdebi-core
 sudo gdebi --non-interactive libsdl1.2debian_1.2.15-5_armhf.deb
 sudo gdebi --non-interactive tingbot-os.deb
 
+# remove the debs, keep $HOME neat and tidy
+rm -f libsdl1.2debian_1.2.15-5_armhf.deb tingbot-os.deb
+
 # run the expand-rootfs tingapp on first boot (the app sets the startup symlink back after running)
 sudo ln -snf ../usr/share/tingbot/expand-rootfs.tingapp /apps/startup
 
